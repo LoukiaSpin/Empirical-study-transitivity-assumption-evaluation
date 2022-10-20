1ab9b7986f4f698faa5f3c202303188d100055a8
Katerina Papadimitropoulou <katerina.papadimitropoulou@gmail.com>
2022-10-20 21:34:26 +0200

    Update Create Figure 4.R

--- R/Create Figure 4.R
+++ R/Create Figure 4.R
@@ -8,31 +8,20 @@
 #* Date: October 2022
 #*******************************************************************************
 
-
-
 ## Install the development version of ggsankey R-package ----
 install.packages("devtools")
 devtools::install_github("davidsjoberg/ggsankey")
-
-
 
 ## Install the development version of rnmamod R-package ----
 #install.packages("devtools")
 #devtools::install_github("LoukiaSpin/rnmamod")
 
-
-
 ## Load libraries ----
 list.of.packages <- c("dplyr", "ggsankey", "stringr")
 lapply(list.of.packages, require, character.only = TRUE); rm(list.of.packages)
 
-
-
 ## Load data ----
 load("./data/judge_trans_quality.RData")  # Saved from R-script 'Create Figure 3'
-
-
-
 
 ## Obtain transitivity quality evaluation ----
 judge_quality <- rep(NA, dim(data_function)[1])
@@ -43,8 +32,6 @@ for (i in 1:dim(data_function)[1]) {
                                     discuss_trans = data_function[i, 4],
                                     proper_table = data_function[i, 5])
 }
-
-
 
 ## Prepare dataset for Sankey plot (ggplot2) ----
 data_sankey0 <- data.frame(data_function,
@@ -57,12 +44,9 @@ colnames(data_sankey0) <-  c("Planned in protocol",
                              "Transitivity evaluation quality")
 
 
-
 ## Turn into wide to be yield the Sankey plot
 data_sankey <- data_sankey0 %>% 
   make_long(colnames(data_sankey0)) 
-
-
 
 ## Draw the Sankey plot (magnificent :-)) ----
 ggplot(data_sankey, 
@@ -83,4 +67,3 @@ ggplot(data_sankey,
   theme(axis.text.x = element_text(size = 12),
         axis.text.y = element_blank(),
         axis.ticks.y = element_blank()) 
-
